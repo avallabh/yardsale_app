9069380074f15bdfa5145728ceae9834cec055e6
Van Michael <van.michael.nguyen@gmail.com>
2014-01-18 13:00:08 -0500

    first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YardsaleApp::Application.config.secret_key_base = '719ee75c14d018827d4f92760dfc95e3d0d86ee76eda198a323d104313256e7ea1f5e9380f148a745eba0542d2f4079fc9d9552146e28e3c03fbde784aa7cb67'
